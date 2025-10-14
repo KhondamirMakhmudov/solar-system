dc55645c50962cf3966cee000e76a79dd9ae3d0f
Xondamir Makhmudov <xondamir.maxmudov.01@mail.ru>
2025-10-14 12:28:42 +0500

    fixed

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 export const config = {
   JAVA_API_URL: "http://10.20.6.129:9090/",
-  PYTHON_API_URL: "http://10.40.9.112:2220/",
+  PYTHON_API_URL: "http://10.40.9.121:2220/",
   // JAVA_API_URL: "http://10.40.9.47:8080/",
   // PYTHON_API_URL: "http://10.40.9.46:1010/ ",
 };
